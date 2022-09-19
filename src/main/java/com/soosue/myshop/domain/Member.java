647daf637980b220e9fcbc5cbdf32f53d95b9e6f
soosue <yunsoo0510@daum.net>
2022-09-19 09:00:45 +0900

    Member 생성

---
+++ src/main/java/com/soosue/myshop/domain/Member.java
@@ -0,0 +1,30 @@
+package com.soosue.myshop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Boolean isShopOwner;
+
+    public Member() {
+    }
+
+    public Member(String name) {
+        this(null, name, Boolean.FALSE);
+    }
+
+    public Member(Long id, String name, Boolean isShopOwner) {
+        this.id = id;
+        this.name = name;
+        this.isShopOwner = isShopOwner;
+    }
+}
